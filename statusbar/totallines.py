e2356b083d9d238baf466bb17e99a720f412ac0f
rajeshsublime <57583490+rajeshsublime@users.noreply.github.com>
2019-11-10 13:33:58 +0530

    Update totallines.py

--- statusbar/totallines.py
+++ statusbar/totallines.py
@@ -1,10 +1,24 @@
-import sublime, sublimeplugin
-  
-class CustomStatusBar(sublimeplugin.Plugin):
-    def onModified(self, view):
-        totalLines = len(view.lines(sublime.Region(0L, view.size())))
-        msg = '%d lines' % (totalLines)
-        sublime.setStatus('totalLines', msg)
+import time
+import sublime
+import sublime_plugin
 
-    def onLoaded(self, view):
-        self.onModified(view)
+last_change = time.time()
+update_interval = 1.5  # s
+
+
+class LineCountUpdateListener(sublime_plugin.EventListener):
+    def update_line_count(self, view):
+        line_count = view.rowcol(view.size())[0] + 1
+        view.set_status("line_count", "#Lines: {0}".format(line_count))
+
+    def on_modified(self, view):
+        global last_change
+        current_change = time.time()
+        # check if we haven't embedded the change in the last update
+        if current_change > last_change + update_interval:
+            last_change = current_change
+            sublime.set_timeout(lambda: self.update_line_count(view),
+                                int(update_interval * 1000))
+
+    on_new = update_line_count
+    on_load = update_line_count
